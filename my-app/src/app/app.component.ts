cb1a4a0a4d69b0cae29c3fc8400e782d0e525933
kevin.xiao <kevin.xiao@3iwom.com>
2017-11-29 22:34:15 +0800

    [feat] install & import primeng/font-awesome

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello my-app';
+  date1: Date;
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { InputTextModule, CalendarModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    InputTextModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
